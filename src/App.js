78338339830eeefe8a9caeeadf615ac53ca8ba9f
HoangTrongTan <137668964+HoangTrongTan@users.noreply.github.com>
2024-05-19 11:51:30 +0700

    ...

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import {publicRoutes}  from "./Routes";
 import { Fragment } from "react";
 import LayoutMain from "./Component/Layout/LayoutMain";
